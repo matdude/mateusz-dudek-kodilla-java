890d7a3ac2ceba0432373f4252787103b03f5990
Mateusz Dudek <mateusz.dudek03@gmail.com>
2020-11-24 15:53:21 +0100

    7.2 ForumUser class added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,25 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String beautifiedText = poemBeautifier.beautify("Text to beautify", (text -> "ABC" + text + "ABC"));
-        System.out.println(beautifiedText);
+      //  PoemBeautifier poemBeautifier = new PoemBeautifier();
+      //  String beautifiedText = poemBeautifier.beautify("Text to beautify", (text -> "ABC" + text + "ABC"));
+      //  System.out.println(beautifiedText);
 
-        String beautifiedText2 = poemBeautifier.beautify("Text to beautify", (text -> text.toUpperCase()));
-        System.out.println(beautifiedText2);
+      //  String beautifiedText2 = poemBeautifier.beautify("Text to beautify", (text -> text.toUpperCase()));
+      //  System.out.println(beautifiedText2);
 
-        String beautifiedText3 = poemBeautifier.beautify("Text to beautify", (text -> text.format("|%20s|", text)));
-        System.out.println(beautifiedText3);
+      //  String beautifiedText3 = poemBeautifier.beautify("Text to beautify", (text -> text.format("|%20s|", text)));
+      //  System.out.println(beautifiedText3);
 
-        String beautifiedText4 = poemBeautifier.beautify("Text to beautify", (text -> "***" + text + "***"));
-        System.out.println(beautifiedText4);
+       // String beautifiedText4 = poemBeautifier.beautify("Text to beautify", (text -> "***" + text + "***"));
+       // System.out.println(beautifiedText4);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
